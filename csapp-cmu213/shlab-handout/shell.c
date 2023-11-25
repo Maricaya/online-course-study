f16c77424d9ef3f1ab8d55a2961f1d2250da849c
Maricaya <915270549@qq.com>
2023-11-25 21:05:44 +0800

    proxy-part1

---
+++ csapp-cmu213/shlab-handout/shell.c
@@ -0,0 +1,161 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MYSH_TOK_DELIM " \t\r\n"
+#define MYSH_TOK_BUFFER_SIZE 64
+
+int mysh_cd(char **args);
+
+int mysh_help(char **args);
+
+int mysh_exit(char **args);
+
+char *builtin_cmd[] = {
+        "cd",
+        "help",
+        "exit"
+};
+
+int mysh_builtin_nums() {
+    return sizeof(builtin_cmd) / sizeof(builtin_cmd[0]);
+};
+
+
+int (*builtin_func[])(char **) = {
+        &mysh_cd,
+        &mysh_help,
+        &mysh_exit,
+};
+
+int mysh_cd(char **args) {
+    if (args[1] == NULL) {
+        perror("Mysh error at cd, lack of args\n");
+    }
+    // cd
+    else {
+        if (chdir(args[1]) != 0) {
+            perror("Mysh error at cd at chdir\n");
+        }
+    }
+    return 1;
+}
+
+int mysh_help(char **args)
+{
+    puts("This is Mt's shell");
+    puts("Here are some built in cmd:");
+    for (int i = 0; i < mysh_builtin_nums(); i ++)
+        printf("%s\n", builtin_cmd[i]);
+    return 1;
+}
+
+int mysh_exit(char **args)
+{
+    return 0;
+}
+
+void mysh_loop();
+
+char *mysh_read_line();
+
+char **mysh_split_line(char *line);
+
+int mysh_execute(char **args);
+
+
+int mysh_launch(char **pString);
+
+void mysh_loop() {
+    char *line;
+    // "cd /desk/main"
+    // "cd" "/desk/main"
+    char **args;
+    int status;
+
+    do {
+        char path[100];
+        getcwd(path, 100);
+        char now[200] = "[mysh ";
+        strcat(now, path);
+        strcat(now, " ]$");
+        printf("%s", now);
+
+        line = mysh_read_line();
+        args = mysh_split_line(line);
+        status = mysh_execute(args);
+
+        free(line);
+        free(args);
+    } while (status);
+
+}
+
+char **mysh_split_line(char *line) {
+    int buffer_size = MYSH_TOK_BUFFER_SIZE, position = 0;
+    char **tokens = malloc(buffer_size * sizeof(char *));
+    char *token;
+
+    // "cd /desk/main"
+    token = strtok(line, MYSH_TOK_DELIM);
+
+    while (token != NULL) {
+        tokens[position++] = token;
+        token = strtok(NULL, MYSH_TOK_DELIM);
+    }
+    tokens[position] = NULL;
+
+    return tokens;
+}
+
+int mysh_launch(char **args) {
+    //    execvp
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+
+    if (pid == 0) {
+        if (execvp(args[0], args) == -1) {
+            perror("Mysh error at execvp\n");
+        }
+        exit(EXIT_FAILURE);
+    } else {
+        do {
+            // 等待指定进程的状态改变，并将状态信息存储在 status 变量中。循环体内的赋值语句
+            waitpid(pid, &status, WUNTRACED);
+//            wpid =
+            // !正常l退出 && !收到信号导致终止
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    return 1;
+};
+
+int mysh_execute(char **args) {
+    if (args[0] == NULL) return 1;
+    for (int i = 0; i < mysh_builtin_nums(); i++) {
+        if (strcmp(args[0], builtin_cmd[i]) == 0) {
+            return (*builtin_func[i])(args);
+        }
+    }
+    // 不是内置命令
+    return mysh_launch(args);
+}
+
+char *mysh_read_line() {
+    char *line = NULL;
+    size_t bufsize;
+    getline(&line, &bufsize, stdin);
+    return line;
+}
+
+
+
+int main(int argc, char *argv[]) {
+    mysh_loop();
+    return 0;
+}
+
+// gcc -c shell.c
+// gcc shell.o -o shell
